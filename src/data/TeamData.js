ff7a12bb502e745cf27fb646e4cc8dadc4300543
theofficialvedantjoshi <vedant.messi101@gmail.com>
2024-11-26 18:42:17 +0530

    fix: fetching senate images from git.

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -26,31 +26,31 @@ export const executives = [
         {
             title: "President",
             name: "Vikhyat Singh",
-            src: "https://media.licdn.com/dms/image/v2/D5603AQEywcL_eBq7RA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1724357588504?e=1732752000&v=beta&t=o935YSUaqnKW5_uOApajwx-9X9B1zgd0IIfeC18xpfw",
+            src: "https://raw.githubusercontent.com/theofficialvedantjoshi/wsc-website/refs/heads/main/src/assets/senate/president.jpeg",
             linkedin: "https://www.linkedin.com/in/vikhyat-singh-a36b3924a/"
         },
         {
             title: "Secretary",
             name: "Aditya Ranjan",
-            src: "https://media.licdn.com/dms/image/v2/D4D03AQFZDNiglFHjiQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1694441482799?e=1732752000&v=beta&t=uXWOcfrveUfR0dlm7p4Fz-BlCw8FLOYn2WHmDOnvbKc",
+            src: "https://raw.githubusercontent.com/theofficialvedantjoshi/wsc-website/refs/heads/main/src/assets/senate/secretary.jpeg",
             linkedin: "https://www.linkedin.com/in/aditya-ranjan-2a686b272/"
         },
         {
             title: "Technical Head",
             name: "Parth Rathi",
-            src: "https://media.licdn.com/dms/image/v2/D4D03AQHGRATJixknOA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1701104556886?e=1732752000&v=beta&t=EWeOxfD7IbQplAyTAgdt_6M0qqR97v157GK54CgI0OA",
+            src: "https://raw.githubusercontent.com/theofficialvedantjoshi/wsc-website/refs/heads/main/src/assets/senate/techhead.jpeg",
             linkedin: "https://www.linkedin.com/in/parth-rathi-070404231/"
         },
         {
             title: "Treasurer",
             name: "Devang Choudhary",
-            src: "https://media.licdn.com/dms/image/v2/D5603AQESHqoDzHvNZQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714657012480?e=1732752000&v=beta&t=zmy9yAYZhmdN8IDYr_EMqwYGmO6YCRzEwkk37T-XgMs",
+            src: "https://raw.githubusercontent.com/theofficialvedantjoshi/wsc-website/refs/heads/main/src/assets/senate/treasurer.jpeg",
             linkedin: "https://www.linkedin.com/in/devang-choudhary-712185230/"
         },
         {
             title: "Marketing Head",
             name: "Vedant Joshi",
-            src: "https://media.licdn.com/dms/image/v2/D5603AQGuaKMcYD0GIA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727939007191?e=1733961600&v=beta&t=k_EO-_xnlIF9-AVbFyXMBPNGjXNsVPFxmHDVoUKMUc8",
+            src: "https://raw.githubusercontent.com/theofficialvedantjoshi/wsc-website/refs/heads/main/src/assets/senate/marketing.jpeg",
             linkedin: "https://www.linkedin.com/in/vedant-joshi-782452281/"
         }
     ];
--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -5,7 +5,7 @@ export default function Default() {
     return (
         <div class="flex flex-col h-screen text-center">
             <Header />
-            <h1 class="text-4xl text-white font-bold mt-28">404 Not Found</h1>
+            <h1 class="text-4xl text-white font-bold mt-56">404 Not Found</h1>
             <p class="text-white mt-4">The page you are looking for does not exist.</p>
         </div>
     )
