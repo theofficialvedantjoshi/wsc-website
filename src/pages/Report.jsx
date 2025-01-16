9436b47a2459bc2ef9177b7d8951cea955862ccf
theofficialvedantjoshi <vedant.messi101@gmail.com>
2025-01-16 14:27:15 +0530

    Fix: Remove pdf preview on mobile devices.

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ParticleBackground from "../components/ParticleBackground";
 export default function Report() {
+    const isPhone = window.innerWidth < 768;
     return (
         <div class="flex flex-col min-h-screen">
             <ParticleBackground />
@@ -10,10 +11,26 @@ export default function Report() {
                 <div class="container px-4 mx-auto text-center mt-28">
                     <h2 class="text-white text-1xl lg:text-3xl md:text-2xl font-bold pt-10 pb-2">Quantitative Finance Report 2023/24</h2>
                     <div class="w-full mt-8">
-                        <iframe src="/report.pdf"
-                            class="w-full h-[500px] lg:h-[800px] border rounded-md"
-                            title="Quantitative Finance Report">
-                        </iframe>
+                        {!isPhone ? (
+                            <iframe src="/report.pdf"
+                                class="w-full h-[500px] lg:h-[800px] border rounded-md"
+                                title="Quantitative Finance Report">
+                            </iframe>
+                        ) : (
+                            <div>
+                                <p class="text-white mb-4">
+                                    PDF preview is not supported on mobile devices. Please download the report below:
+                                </p>
+                                <a
+                                    href="/report.pdf"
+                                    download
+                                    class="text-red-900 underline font-bold"
+                                >
+                                    Download Report (PDF)
+                                </a>
+                            </div>
+                        )
+                        }
                     </div>
                 </div>
             </div>
